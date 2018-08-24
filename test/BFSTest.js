861e0963a50aeffc1662e6906fc28cb0d2e8b4de
Vidovitsch <david.de.bekker@gmail.com>
2018-08-24 11:56:17 +0200

    ESLint comments removed

--- test/BFSTest.js
+++ test/BFSTest.js
@@ -2,7 +2,6 @@ const { assert } = require('chai');
 const { BFS } = require('../index');
 const Graph = require('../util/Graph');
 
-/* eslint-disable no-undef */
 describe('BreadthFirstSearch', () => {
   it("BFS object should process the graph at initiation)", () => {
     const source = 'A';
@@ -98,4 +97,3 @@ describe('BreadthFirstSearch', () => {
     assert.equal(bfs.edgeTo['B'], null);
   });
 });
-/* eslint-enable no-undef */
